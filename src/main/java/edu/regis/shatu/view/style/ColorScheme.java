ffc46be9e97eeabbac983360b0b4bf8957b1e8cc
Henry Ordonez <h.ordonez9@gmail.com>
2025-10-12 20:14:02 -0600

    Centralize color values inside ColorScheme.java

---
+++ src/main/java/edu/regis/shatu/view/style/ColorScheme.java
@@ -0,0 +1,28 @@
+/*
+ *  SHATU: SHA-256 Tutor
+ * 
+ *   (C) Johanna & Richard Blumenthal, All rights reserved
+ * 
+ *   Unauthorized use, duplication or distribution without the authors'
+ *   permission is strictly prohibited.
+ * 
+ *   Unless required by applicable law or agreed to in writing, this
+ *   software is distributed on an "AS IS" basis without warranties
+ *   or conditions of any kind, either expressed or implied.
+ */
+package edu.regis.shatu.view.style;
+
+import java.awt.Color;
+
+/**
+ * Contains the colors used throughout the application.
+ *
+ * @author Henry Ordonez
+ */
+public class ColorScheme {
+    public static final Color REGIS_BLUE = new Color(0, 43, 73);
+    public static final Color REGIS_YELLOW = new Color(241, 196, 0);
+    public static final Color REGIS_BLUE_PASTEL = new Color(102, 153, 204);
+    public static final Color REGIS_YELLOW_PASTEL = new Color(255, 223, 128);
+    public static final Color WHITE = Color.WHITE;
+}
